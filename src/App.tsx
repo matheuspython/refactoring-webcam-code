9f80a638addfb202a538a89d54dfa0a380a831c2
matheus souza <64442354+matheuspython@users.noreply.github.com>
2023-11-09 18:28:44 -0300

    Add files via upload
    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Styles } from './globals/styles'
+
+export const App:React.FC = () => {
+
+  return (
+    <>
+     <Styles />
+     oi
+    </>
+  )
+}
+
---
+++ src/globals/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Styles = createGlobalStyle`
+   *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html, body{
+    font-family: 'Roboto', sans-serif;
+  }
+`
